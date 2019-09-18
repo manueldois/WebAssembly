4d214c55e20e6dca76d448ebc50bf49004debb49
MaxGraey <maxgraey@gmail.com>
2019-09-19 02:12:34 +0300

    improve sort colors bench

--- Sort Colors Benchmark/src/assembly/index.ts
+++ Sort Colors Benchmark/src/assembly/index.ts
@@ -1,98 +1,86 @@
 // The entry file of your WebAssembly module.
 memory.grow(1);
 
+@inline
 function getPixelRGB(X: i32, Y: i32, WIDTH: i32, DATA_POINTER: i32): u32 {
-  const PIXEL_INDEX = Y * WIDTH * 4 + X * 4
-  const R = load<u8>(DATA_POINTER + PIXEL_INDEX + 0)
-  const G = load<u8>(DATA_POINTER + PIXEL_INDEX + 1)
-  const B = load<u8>(DATA_POINTER + PIXEL_INDEX + 2)
-  const A = load<u8>(DATA_POINTER + PIXEL_INDEX + 3)
-  return (<u32>R << 16) + (<u32>G << 8) + (<u32>B)
+  const PIXEL_INDEX = (Y * WIDTH + X) << 2
+  return load<u32>(DATA_POINTER + PIXEL_INDEX);
 }
 
-function setPixelRGB(X: i32, Y: i32, WIDTH: i32, DATA_POINTER: i32, RGB: u32): void {
-  const PIXEL_INDEX = Y * WIDTH * 4 + X * 4
-  const R = (RGB >> 16)
-  const G = (RGB - (R << 16)) >> 8
-  const B = (RGB - (R << 16) - (G << 8))
-
-  store<u8>(DATA_POINTER + PIXEL_INDEX + 0, R)
-  store<u8>(DATA_POINTER + PIXEL_INDEX + 1, G)
-  store<u8>(DATA_POINTER + PIXEL_INDEX + 2, B)
-  store<u8>(DATA_POINTER + PIXEL_INDEX + 3, 255)
+@inline
+function setPixelRGB(X: i32, Y: i32, WIDTH: i32, DATA_POINTER: i32, RGBA: u32): void {
+  const PIXEL_INDEX = (Y * WIDTH + X) << 2
+  store<u32>(DATA_POINTER + PIXEL_INDEX, RGBA)
 }
 
 /**
- * 
+ *
  * @param R Red as u8
  * @param G Green as u8
  * @param B Blue as u8
  * @returns HSL as u32. H, S, L range (0, 255). H is left shifted 16bits, S 8bits, and L 0bits
  */
 function rgbToHsl(RGB: u32): u32 {
-  const R = (RGB >> 16)
-  const G = (RGB - (R << 16)) >> 8
-  const B = (RGB - (R << 16) - (G << 8))
+  const R = RGB & 0xFF;
+  const G = (RGB >> 8) & 0xFF;
+  const B = (RGB >> 16) & 0xFF;
 
-  let R_f32 = (<f32>R) / 255
-  let G_f32 = (<f32>G) / 255
-  let B_f32 = (<f32>B) / 255;
+  let R_f32 = (<f32>R) * (1.0 / 255)
+  let G_f32 = (<f32>G) * (1.0 / 255)
+  let B_f32 = (<f32>B) * (1.0 / 255)
 
-  let max_GB = <f32>Math.max(G_f32, B_f32)
-  let max = <f32>Math.max(max_GB, R_f32)
-  let min_GB = <f32>Math.min(G_f32, B_f32)
-  let min = <f32>Math.min(min_GB, R_f32)
+  let max = Mathf.max(Mathf.max(G_f32, B_f32), R_f32)
+  let min = Mathf.min(Mathf.min(G_f32, B_f32), R_f32)
 
-  let H: f32 = 0, S: f32 = 0, L: f32 = (max + min) / 2;
+  let H: f32 = 0, S: f32 = 0, L: f32 = (max + min) * 0.5;
 
-  if (max === min) {
+  if (max == min) {
     H = S = 0; // achromatic
   } else {
     var d = max - min;
     S = L > 0.5 ? d / (2 - max - min) : d / (max + min);
+    d = 1 / d;
 
     if (max === R_f32) {
-      H = (G_f32 - B_f32) / d + (G_f32 < B_f32 ? 6 : 0)
+      H = (G_f32 - B_f32) * d + (G_f32 < B_f32 ? 6 : 0)
     } else {
       if (max === G_f32) {
-        H = (B_f32 - R_f32) / d + 2
+        H = (B_f32 - R_f32) * d + 2
       } else {
         if (max === B_f32) {
-          H = (R_f32 - G_f32) / d + 4;
+          H = (R_f32 - G_f32) * d + 4;
         }
       }
     }
 
-    H = H / 6;
+    H *= 1.0 / 6.0;
   }
 
-  return <u32>((<u32>(H * 255) << 16) + (<u32>(S * 255) << 8) + <u32>(L * 255))
+  return (<u32>(H * 255) << 16) | (<u32>(S * 255) << 8) | <u32>(L * 255)
 }
 
 export { getPixelRGB, setPixelRGB, rgbToHsl }
 
 export function sortImgData(DATA_POINTER: i32, IMG_SIZE: i32): void {
-  const WIDTH: i32 = IMG_SIZE
-  const HEIGHT: i32 = IMG_SIZE
-  let didChanges: u8 = 0
+  const WIDTH = IMG_SIZE
+  const HEIGHT = IMG_SIZE
+  let didChanges = false
 
   do {
-    didChanges = 0
-    for (let y: i32 = 0; y < HEIGHT; y++) {
-      for (let x: i32 = 0; x < WIDTH; x++) {
+    didChanges = false
+    for (let y = 0; y < HEIGHT; y++) {
+      for (let x = 0; x < WIDTH; x++) {
         const PIXEL_B = getPixelRGB(x + 1, y, WIDTH, DATA_POINTER)
         const PIXEL_A = getPixelRGB(x, y, WIDTH, DATA_POINTER)
-          
-          const PIXEL_B_HUE = rgbToHsl(PIXEL_B) >> 16
-          const PIXEL_A_HUE = rgbToHsl(PIXEL_A) >> 16
+        const PIXEL_B_HUE = rgbToHsl(PIXEL_B) >> 16
+        const PIXEL_A_HUE = rgbToHsl(PIXEL_A) >> 16
 
         if (PIXEL_B_HUE > PIXEL_A_HUE) {
           setPixelRGB(x, y, WIDTH, DATA_POINTER, PIXEL_B)
           setPixelRGB(x + 1, y, WIDTH, DATA_POINTER, PIXEL_A)
-          didChanges = 1
+          didChanges = true
         }
       }
     }
-  } while (didChanges === 1);
-
+  } while (didChanges);
 }
